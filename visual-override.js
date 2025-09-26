2d997569e78aeb776f35e9c46600bcad79a640a5
Dzhygun <5046351+dzhygun@users.noreply.github.com>
2025-09-26 17:20:06 +0200

    fixed styles formatting; changed default theme to dark

--- visual-override.js
+++ visual-override.js
@@ -3,36 +3,32 @@
  */
 
 var generateOverride = function (params) {
-       let output = '';
+       let output = `
+
+`;
 
        let textColorLightInput = null;
        let textColorDarkInput = null;
        if (params.textColorLight !== '#D73A42') {
-              textColorLightInput = `
-      
-        input[type=checkbox]:checked + label:before{
-               background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 11 8'%3e%3cpolygon points='9.53 0 4.4 5.09 1.47 2.18 0 3.64 2.93 6.54 4.4 8 5.87 6.54 11 1.46 9.53 0' fill='${params.textColorLight.replace('#', '%23')}'/%3e%3c/svg%3e");
-        }
+              textColorLightInput = 
+`input[type=checkbox]:checked + label:before{
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 11 8'%3e%3cpolygon points='9.53 0 4.4 5.09 1.47 2.18 0 3.64 2.93 6.54 4.4 8 5.87 6.54 11 1.46 9.53 0' fill='${params.textColorLight.replace('#', '%23')}'/%3e%3c/svg%3e");
+}
 
-        input[type=radio]:checked + label:before {
-               background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3ccircle cx='4' cy='4' r='4' fill='${params.textColorLight.replace('#', '%23')}'/%3e%3c/svg%3e");
-        
-
-        }`;
+input[type=radio]:checked + label:before {
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3ccircle cx='4' cy='4' r='4' fill='${params.textColorLight.replace('#', '%23')}'/%3e%3c/svg%3e");
+}`;
        }
 
        if (params.textColorDark !== '#D73A42') {
-              textColorDarkInput = `
-      
-        input[type=checkbox]:checked + label:before{
-               background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 11 8'%3e%3cpolygon points='9.53 0 4.4 5.09 1.47 2.18 0 3.64 2.93 6.54 4.4 8 5.87 6.54 11 1.46 9.53 0' fill='${params.textColorDark.replace('#', '%23')}'/%3e%3c/svg%3e");
-        }
+              textColorDarkInput = 
+`input[type=checkbox]:checked + label:before{
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 11 8'%3e%3cpolygon points='9.53 0 4.4 5.09 1.47 2.18 0 3.64 2.93 6.54 4.4 8 5.87 6.54 11 1.46 9.53 0' fill='${params.textColorDark.replace('#', '%23')}'/%3e%3c/svg%3e");
+}
 
-        input[type=radio]:checked + label:before {
-               background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3ccircle cx='4' cy='4' r='4' fill='${params.textColorDark.replace('#', '%23')}'/%3e%3c/svg%3e");
-        
-
-        }`;
+input[type=radio]:checked + label:before {
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3ccircle cx='4' cy='4' r='4' fill='${params.textColorDark.replace('#', '%23')}'/%3e%3c/svg%3e");
+}`;
        }
 
        if (params.colorScheme === 'dark') {
@@ -44,45 +40,46 @@ var generateOverride = function (params) {
                      output += textColorLightInput;
                      if (params.colorScheme === 'auto') {
                             output +=     ` 
-                                          @media (prefers-color-scheme: dark) {
-                                                 :root {                
-                                                 ${textColorDarkInput}
-                                                 }        
-                                          }
-                                          `;
+                            
+@media (prefers-color-scheme: dark) {
+    :root {
+${textColorDarkInput}
+    }        
+}
+`;
                      }
               }
        }
 
        if (params.submenu === 'custom') {
               output += `
-        .navbar .navbar__submenu {
-              white-space: wrap;
-               width: ${params.submenuWidth}px;     
-        }
-        .navbar .has-submenu .has-submenu:active > .navbar__submenu,
-        .navbar .has-submenu .has-submenu:focus > .navbar__submenu,
-        .navbar .has-submenu .has-submenu:hover > .navbar__submenu {
-               left: ${params.submenuWidth}px;  
-        }
-        .navbar .has-submenu .has-submenu:active > .navbar__submenu.is-right-submenu,
-        .navbar .has-submenu .has-submenu:focus > .navbar__submenu.is-right-submenu,
-        .navbar .has-submenu .has-submenu:hover > .navbar__submenu.is-right-submenu {
-               left: -${params.submenuWidth}px; 
-        }`;
+.navbar .navbar__submenu {
+    white-space: wrap;
+    width: ${params.submenuWidth}px;     
+}
+.navbar .has-submenu .has-submenu:active > .navbar__submenu,
+.navbar .has-submenu .has-submenu:focus > .navbar__submenu,
+.navbar .has-submenu .has-submenu:hover > .navbar__submenu {
+    left: ${params.submenuWidth}px;  
+}
+.navbar .has-submenu .has-submenu:active > .navbar__submenu.is-right-submenu,
+.navbar .has-submenu .has-submenu:focus > .navbar__submenu.is-right-submenu,
+.navbar .has-submenu .has-submenu:hover > .navbar__submenu.is-right-submenu {
+    left: -${params.submenuWidth}px; 
+}`;
        }
 
 
        if (params.lazyLoadEffect === 'fadein') {
               output += ` 
-         img[loading] {
-               opacity: 0;
-         }
+img[loading] {
+    opacity: 0;
+}
 
-         img.is-loaded {
-               opacity: 1;
-               transition: opacity 1s cubic-bezier(0.215, 0.61, 0.355, 1); 
-         }`;
+img.is-loaded {
+    opacity: 1;
+    transition: opacity 1s cubic-bezier(0.215, 0.61, 0.355, 1); 
+}`;
        }
 
 
